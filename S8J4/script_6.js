d161e952031dc8737a4168860dcb5ecba7cf875f
matthieu-bouquin <113907755+matthieu-bouquin@users.noreply.github.com>
2022-11-18 10:59:42 +0100

    Update script_6.js

--- S8J4/script_6.js
+++ S8J4/script_6.js
@@ -23,19 +23,10 @@ const ARN = {
   UAC: 'Tyrosine',
 };
 function convertARNToAcid(str) {
-  let arr = str.toUpperCase().match(/.{1,3}/g);
-  let result = [];
-  arr.forEach((item) => {
-    result.push(ARN[item]);
-  });
 
-  return result.join('-');
-}
-// Supprime le livre avec l'ID: 133712 ;
-console.log('\n----------------------------------------------------');
+
 console.log('ARN de CCGUCGUUGCGCUACAGC est: ');
 console.log(convertARNToAcid('CCGUCGUUGCGCUACAGC'));
 
-console.log('\n----------------------------------------------------');
 console.log('ARN de CCUCGCCGGUACUUCUCG est: ');
 console.log(convertARNToAcid('CCUCGCCGGUACUUCUCG'));
